ec557c6ff923f55062f8f076e4ea7517f38ebbed
sgilb <32495429+sgilb@users.noreply.github.com>
2023-12-04 09:48:49 +0000

    Add function to create custom-sized grid

---
+++ script.js
@@ -0,0 +1,19 @@
+function createGrid(size) {
+  const container = document.getElementById("container");
+  
+  for (let i = 0; i < size; i++) {
+    let row = document.createElement("div");
+    row.className = "row";
+
+    for (let j = 1; j <= size; j++) {
+      let cell = document.createElement("div");
+      cell.className = "grid-cell";
+      cell.innerText = (i * size) + j;
+      row.appendChild(cell);
+    }
+
+    container.appendChild(row);
+  }
+}
+
+createGrid(5);
