2ce85f7704aef3ea6b12f15b606023fc7d1b68e2
sgilb <32495429+sgilb@users.noreply.github.com>
2023-12-27 21:02:07 +0000

    Add rainbow mode 🌈

--- script.js
+++ script.js
@@ -34,11 +34,25 @@ function clearGrid() {
   }
 }
 
+function getRandomPaintColor() {
+  const randomBetween = (min, max) =>
+    min + Math.floor(Math.random() * (max - min + 1));
+  const r = randomBetween(0, 255);
+  const g = randomBetween(0, 255);
+  const b = randomBetween(0, 255);
+  return `rgb(${r},${g},${b})`;
+}
+
 const grid = document.getElementById("container");
 grid.addEventListener("mouseover", (e) => {
   if (e.target && e.target.matches(".grid-cell")) {
     const hoveredCell = e.target;
-    hoveredCell.style.backgroundColor = "black";
+
+    if (e.buttons === 1) {
+      hoveredCell.style.backgroundColor = getRandomPaintColor();
+    } else {
+      hoveredCell.style.backgroundColor = "black";
+    }
   }
 });
 
