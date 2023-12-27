56400a98355e17a99067c69ef2eeaac814539e08
sgilb <32495429+sgilb@users.noreply.github.com>
2023-12-27 20:29:45 +0000

    Add ability to clear existing canvas

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function createGrid(size) {
   const container = document.getElementById("container");
-  
+
   for (let i = 0; i < size; i++) {
     let row = document.createElement("div");
     row.className = "row";
@@ -14,29 +14,41 @@ function createGrid(size) {
     container.appendChild(row);
   }
 }
-createGrid(16);
+createGrid(64);
 
 function resetGrid(size) {
+  if (size === null) return;
   if (typeof Number(size) !== "number" || size <= 0 || size > 100) {
     alert("Please enter a number between 1 and 100");
-  }
-  else {
+  } else {
     const container = document.getElementById("container");
     container.innerHTML = ""; // Clear existing grid
     createGrid(size);
-  } 
+  }
+}
+
+function clearGrid() {
+  const cells = document.querySelectorAll(".grid-cell");
+  for (i = 0; i < cells.length; i++) {
+    cells[i].style.backgroundColor = "white";
+  }
 }
 
 const grid = document.getElementById("container");
-grid.addEventListener("mouseover", e => {
+grid.addEventListener("mouseover", (e) => {
   if (e.target && e.target.matches(".grid-cell")) {
     const hoveredCell = e.target;
-    hoveredCell.style.backgroundColor = "red"
+    hoveredCell.style.backgroundColor = "black";
   }
 });
 
 const newCanvasButton = document.getElementById("reset-grid");
-newCanvasButton.addEventListener("click", e => {
+newCanvasButton.addEventListener("click", (e) => {
   const gridSize = prompt("Enter a grid size (max. 100)");
   resetGrid(gridSize);
-})
+});
+
+const clearCanvasButton = document.getElementById("clear-grid");
+clearCanvasButton.addEventListener("click", (e) => {
+  clearGrid();
+});
